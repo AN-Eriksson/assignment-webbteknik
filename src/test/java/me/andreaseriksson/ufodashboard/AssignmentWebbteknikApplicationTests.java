f534f97bffbd84e7b06c5f6e03ea723e31fa4051
Andreas Eriksson <ae225ak@student.lnu.se>
2026-04-21 11:44:34 +0200

    Initial commit after Initializer

---
+++ src/test/java/me/andreaseriksson/ufodashboard/AssignmentWebbteknikApplicationTests.java
@@ -0,0 +1,13 @@
+package me.andreaseriksson.ufodashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentWebbteknikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
