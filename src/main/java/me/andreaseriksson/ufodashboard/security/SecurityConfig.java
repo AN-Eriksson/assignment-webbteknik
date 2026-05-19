93b79fc949990058676f0c915a31550afbf53bbf
Andreas Eriksson <ae225ak@student.lnu.se>
2026-05-19 11:36:41 +0200

    Add missing attribute @EnableMethodSecurity

--- src/main/java/me/andreaseriksson/ufodashboard/security/SecurityConfig.java
+++ src/main/java/me/andreaseriksson/ufodashboard/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,6 +17,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Value("${app.cors.allowed-origins:http://localhost:5173,http://localhost:8080}")
